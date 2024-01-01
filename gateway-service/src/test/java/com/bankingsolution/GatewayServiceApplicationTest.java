d7ab02d6e9cc45fc75be5ce64975d88bc009ce54
anton <antonkuchisnky86@gmail.com>
2024-01-01 20:11:21 +0300

    add discovery and gateway services

---
+++ gateway-service/src/test/java/com/bankingsolution/GatewayServiceApplicationTest.java
@@ -0,0 +1,13 @@
+package com.bankingsolution;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
