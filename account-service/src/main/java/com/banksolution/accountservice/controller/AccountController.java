a4404371d9279994e1f564cb11e3d1703b42c8ec
anton <antonkuchisnky86@gmail.com>
2024-01-05 15:42:14 +0300

    add controller

---
+++ account-service/src/main/java/com/banksolution/accountservice/controller/AccountController.java
@@ -0,0 +1,59 @@
+package com.banksolution.accountservice.controller;
+
+import com.banksolution.accountservice.dto.AccountDto;
+import com.banksolution.accountservice.dto.AccountStatusDto;
+import com.banksolution.accountservice.dto.AccountTypeDto;
+import com.banksolution.accountservice.dto.transactional.BalanceOperationDto;
+import com.banksolution.accountservice.dto.transactional.TransferMoneyDto;
+import com.banksolution.accountservice.service.AccountService;
+import com.banksolution.accountservice.service.TransactionalService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("api/v1/account")
+@RequiredArgsConstructor
+public class AccountController {
+    private final AccountService accountService;
+    private final TransactionalService transactionalService;
+
+    @GetMapping("{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public BigDecimal getBalance(@PathVariable UUID id){
+        return accountService.getBalance(id);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createAccount(AccountDto accountDto){
+      accountService.createAccount(accountDto);
+    }
+
+    @PatchMapping("/change/type")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void changeAccountType(AccountTypeDto accountTypeDto){
+        accountService.changeAccountType(accountTypeDto);
+    }
+
+    @PatchMapping("/change/status")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void changeAccountStatus(AccountStatusDto accountStatusDto){
+        accountService.changeAccountStatus(accountStatusDto);
+    }
+
+    @PatchMapping("/transaction/transfer")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void transferFundsBetweenAccounts(TransferMoneyDto transferMoneyDto){
+     transactionalService.transferFundsBetweenAccounts(transferMoneyDto);
+    }
+
+    @PatchMapping("/transaction/refill")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void refillBalance(BalanceOperationDto balanceOperationDto){
+        transactionalService.refillBalance(balanceOperationDto);
+    }
+}
