4ba0e0bf31541f381afed57f6e82bfda67a161ce
anton <antonkuchisnky86@gmail.com>
2024-01-09 17:27:05 +0300

    change currency variable only USD

--- account-service/src/main/java/com/banksolution/accountservice/dto/transactional/TransferMoneyDto.java
+++ account-service/src/main/java/com/banksolution/accountservice/dto/transactional/TransferMoneyDto.java
@@ -1,6 +1,7 @@
 package com.banksolution.accountservice.dto.transactional;
 
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import org.jetbrains.annotations.NotNull;
 
 import java.math.BigDecimal;
@@ -9,6 +10,8 @@ import java.util.UUID;
 public record TransferMoneyDto(
         UUID senderId,
         UUID recepientId,
+        @Pattern(regexp = "USD", message = "The currency can be 'USD'")
+        String currency,
         @NotNull
         @NotEmpty
         BigDecimal money
