2bff1fb413ff2e861ca02c096e599844eee49ea0
anton <antonkuchisnky86@gmail.com>
2024-01-11 22:21:10 +0300

    add exception classes

---
+++ transaction-service/src/main/java/com/banksolution/transactionservice/exception/InvalidDataException.java
@@ -0,0 +1,15 @@
+package com.banksolution.transactionservice.exception;
+
+public class InvalidDataException extends RuntimeException{
+
+    private final String title;
+
+    public InvalidDataException(String message, String title) {
+        super(message);
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
