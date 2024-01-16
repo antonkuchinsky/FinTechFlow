cd6a5e3007b4a30e92fd60d83cef40ae22ea0f9e
anton <antonkuchisnky86@gmail.com>
2024-01-16 17:11:20 +0300

    add topic for refill balance

--- transaction-service/src/main/java/com/banksolution/transactionservice/config/Consumer.java
+++ transaction-service/src/main/java/com/banksolution/transactionservice/config/Consumer.java
@@ -12,12 +12,20 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class Consumer {
 
-    private static final String regTopic = "${topic.name}";
+    private static final String regTopicOfAccount = "${topic_of_accounts.name}";
+    private static final String regTopicOfRefillBalance="${topic_refill_balance.name}";
 
     private final ObjectMapper objectMapper;
     private final TransactionService transactionService;
 
-    @KafkaListener(topics = regTopic)
+    @KafkaListener(topics = regTopicOfAccount)
+    public void consumeMessageOfAccounts(String message) throws JsonProcessingException {
+
+        var transaction = objectMapper.readValue(message, Transaction.class);
+        transactionService.saveTransaction(transaction);
+    }
+
+    @KafkaListener(topics = regTopicOfRefillBalance)
     public void consumeMessage(String message) throws JsonProcessingException {
 
         var transaction = objectMapper.readValue(message, Transaction.class);
